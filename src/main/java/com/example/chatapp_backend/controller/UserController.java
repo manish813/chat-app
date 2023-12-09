504745632a15bbb88d2d19bf48253dbbf097d386
manish813 <manishnaik500@gmail.com>
2023-12-09 12:33:56 +0530

    Created api's for authentication and provided jwt support

--- src/main/java/com/example/chatapp_backend/controller/UserController.java
+++ src/main/java/com/example/chatapp_backend/controller/UserController.java
@@ -1,4 +1,48 @@
 package com.example.chatapp_backend.controller;
 
+import com.example.chatapp_backend.dto.JwtResponseDTO;
+import com.example.chatapp_backend.dto.UsersResponseDto;
+import com.example.chatapp_backend.dto.UserRequestDto;
+import com.example.chatapp_backend.service.JwtService;
+import com.example.chatapp_backend.service.UsersService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("users")
 public class UserController {
+
+    private UsersService usersService;
+    private AuthenticationManager authenticationManager;
+    private JwtService jwtService;
+    public UserController(UsersService usersService, AuthenticationManager authenticationManager, JwtService jwtService) {
+        this.usersService = usersService;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+    }
+    @PostMapping("/register")
+    public UsersResponseDto registerUser(@RequestBody UserRequestDto request) {
+        return usersService.addUsers(request);
+    }
+
+    @PostMapping("/login")
+    public JwtResponseDTO authenticateAndGetToken(@RequestBody UserRequestDto userRequestDto) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userRequestDto.getUserName(),userRequestDto.getPassword()));
+        if(authentication.isAuthenticated()) {
+            return new JwtResponseDTO(jwtService.generateToken(userRequestDto.getUserName()));
+        } else {
+            throw new UsernameNotFoundException("invalid user request..!!");
+        }
+    }
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return new ResponseEntity<>("Ping", HttpStatus.OK);
+    }
 }
---
+++ src/main/java/com/example/chatapp_backend/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.chatapp_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+}
